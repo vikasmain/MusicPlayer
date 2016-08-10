d62a9ec8629920e85706b11fab23d9c93d94fc67
Vikas Bajpayee <Vikas Bajpayee>
2016-08-10 06:54:13 +0530

    Initial commit

---
+++ app/src/main/java/com/example/dell/musicplayer/activities/DetailActivity.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2016. André Mion
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.dell.musicplayer.activities;
+
+import android.os.Bundle;
+import android.transition.Transition;
+import android.view.View;
+
+import com.example.dell.musicplayer.R;
+import com.example.dell.musicplayer.view.CoverView;
+import com.example.dell.musicplayer.view.TransitionAdapter;
+
+public class DetailActivity extends PlayerActivity {
+
+    private CoverView mCoverView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_detail);
+
+        mCoverView = (CoverView) findViewById(R.id.cover);
+        mCoverView.setCallbacks(new CoverView.Callbacks() {
+            @Override
+            public void onStopAnimation() {
+                supportFinishAfterTransition();
+            }
+        });
+
+        getWindow().getSharedElementEnterTransition().addListener(new TransitionAdapter() {
+            @Override
+            public void onTransitionEnd(Transition transition) {
+                play();
+                mCoverView.start();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        onFabClick(null);
+    }
+
+    public void onFabClick(View view) {
+        pause();
+        mCoverView.stop();
+    }
+
+}
---
+++ app/src/main/java/com/example/dell/musicplayer/view/RecyclerViewAdapter.java
@@ -0,0 +1,73 @@
+package com.example.dell.musicplayer.view;
+
+import android.support.v7.widget.RecyclerView;
+import android.text.format.DateUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.dell.musicplayer.R;
+import com.example.dell.musicplayer.music.MusicContent;
+
+import java.util.List;
+
+;
+
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+
+    private final List<MusicContent.MusicItem> mValues;
+
+    public RecyclerViewAdapter(List<MusicContent.MusicItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.content_list_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mCoverView.setImageResource(holder.mItem.getCover());
+        holder.mTitleView.setText(holder.mItem.getTitle());
+        holder.mArtistView.setText(holder.mItem.getArtist());
+        holder.mDurationView.setText(DateUtils.formatElapsedTime(holder.mItem.getDuration()));
+
+        holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Nothing to do
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mCoverView;
+        public final TextView mTitleView;
+        public final TextView mArtistView;
+        public final TextView mDurationView;
+        public MusicContent.MusicItem mItem;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mCoverView = (ImageView) view.findViewById(R.id.cover);
+            mTitleView = (TextView) view.findViewById(R.id.title);
+            mArtistView = (TextView) view.findViewById(R.id.artist);
+            mDurationView = (TextView) view.findViewById(R.id.duration);
+        }
+    }
+
+}
